6d20d408a691f654f70a5900292bc32fb67ee95a
popo63301 <popo63301@gmail.com>
2018-11-27 08:19:50 +0100

    UI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div>toto</div>;
+    return (
+      <div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: '50px'
+          }}
+        >
+          <div style={{ fontSize: '40px', fontWeight: 'bold' }}> 0</div>
+          <div>
+            <button>+</button>
+            <button>-</button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
